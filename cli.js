1e9029df7dee4dfb89f23d9d1e1894caf52ba5ef
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-23 23:08:40 +0200

    Implements cli.js

---
+++ cli.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var seeiendom = require('./index');
+var query = process.argv[2];
+var pkg = require('./package.json');
+var options = {
+  query: query
+};
+
+if (query === '-v' || query === '--version') {
+  return console.log(pkg.version);
+}
+
+if (query === '-h' || query === '--help' || query === undefined) {
+  console.log('seeiendom ' + pkg.version);
+  console.log('Usage:');
+  console.log('Search seeiendom.no. Pass in your query.');
+  return console.log('$ seeiendom 0806-60/77');
+}
+
+seeiendom(options, function(error, data) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
