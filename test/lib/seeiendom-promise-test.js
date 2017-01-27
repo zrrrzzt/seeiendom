bcbdb3c1381cc65c6e1e7ea4b54bdbfe6714b5c2
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-27 20:40:59 +0100

    Adds test for promises interface

---
+++ test/lib/seeiendom-promise-test.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const tap = require('tap')
+const seeiendom = require('../../index')
+
+tap.test('it requires an options object', function (test) {
+  const options = false
+  const expectedErrorMessage = 'Missing required param: options'
+  seeiendom(options)
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((error) => {
+      tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+      test.done()
+    })
+})
+
+tap.test('it requires options.query to exist', function (test) {
+  const options = {
+    query: false
+  }
+  const expectedErrorMessage = 'Missing required param: options.query'
+  seeiendom(options)
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((error) => {
+      tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+      test.done()
+    })
+})
+
+tap.test('it returns expected data', function (test) {
+  const options = {
+    sources: ['sted', 'matreiendom'],
+    key: 'httpwwwseeiendomno',
+    groups: ['guests'],
+    query: '0806-60/77'
+  }
+  const expectedData = '41515792'
+  seeiendom(options)
+    .then((data) => {
+      tap.equal(data[0].ID.toString(), expectedData, 'Data ok')
+      test.done()
+    })
+    .catch((error) => {
+      throw error
+    })
+})
