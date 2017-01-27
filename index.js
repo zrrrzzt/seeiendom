6cfd4b637d3b12100053f2d321f21565dd1c92f0
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-27 20:30:52 +0100

    Implements promises interface

--- index.js
+++ index.js
@@ -6,31 +6,47 @@ const buildFilterstring = require('./lib/buildFilterstring')
 const apiUrl = 'http://eiendom.statkart.no/Search.ashx'
 
 module.exports = (options, callback) => {
-  if (!options) {
-    return callback(new Error('Missing required param: options'), null)
-  }
-  if (!options.query) {
-    return callback(new Error('Missing required param: options.query'), null)
-  }
-  const filterQuery = {
-    sources: options.sources || ['sted', 'matreiendom'],
-    key: options.key || 'httpwwwseeiendomno',
-    groups: options.groups || ['guests']
-  }
-  var query = {
-    'term': options.query,
-    '_': new Date().getTime()
-  }
-
-  query.filter = buildFilterstring(filterQuery)
-  const qs = querystring.stringify(query)
-  const url = `${apiUrl}?${qs}`
-
-  getData(url, (err, data) => {
-    if (err) {
-      return callback(err, null)
-    } else {
-      return callback(null, data)
+  return new Promise((resolve, reject) => {
+    if (!options) {
+      let error = new Error('Missing required param: options')
+      if (callback) {
+        return callback(error, null)
+      }
+      reject(error)
     }
+    if (!options.query) {
+      let error = new Error('Missing required param: options.query')
+      if (callback) {
+        return callback(error, null)
+      }
+      reject(error)
+    }
+    const filterQuery = {
+      sources: options.sources || ['sted', 'matreiendom'],
+      key: options.key || 'httpwwwseeiendomno',
+      groups: options.groups || ['guests']
+    }
+    let query = {
+      'term': options.query,
+      '_': new Date().getTime()
+    }
+
+    query.filter = buildFilterstring(filterQuery)
+    const qs = querystring.stringify(query)
+    const url = `${apiUrl}?${qs}`
+
+    getData(url, (error, data) => {
+      if (error) {
+        if (callback) {
+          return callback(error, null)
+        }
+        reject(error)
+      } else {
+        if (callback) {
+          return callback(null, data)
+        }
+        resolve(data)
+      }
+    })
   })
 }
